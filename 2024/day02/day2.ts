c0336a91d10cb096e56e2fd33076e1c7c636986b
Holly <103004659+Hollerina@users.noreply.github.com>
2024-12-02 21:37:54 +0000

    day 2 part 2

--- 2024/day02/day2.ts
+++ 2024/day02/day2.ts
@@ -5,7 +5,7 @@ const input = fs.readFileSync('./day02/day2.txt', 'utf-8').split("\n");
 const increasingNumbers = (numberArray: string[]) => {
     let state: string = "Safe"
     for(let i = 1; i < numberArray.length - 1; i++) {
-        if(Number(numberArray[i]) - Number(numberArray[i + 1]) > 3 || Number(numberArray[i]) <= Number(numberArray[i + 1])) {
+        if(Number(numberArray[i + 1]) - Number(numberArray[i]) > 3 || Number(numberArray[i]) >= Number(numberArray[i + 1])) {
             state = "NotSafe"
             break
         }
@@ -16,7 +16,7 @@ const increasingNumbers = (numberArray: string[]) => {
 const decreasingNumbers = (numberArray: string[]) => {
     let state: string = "Safe"
     for(let i = 1; i < numberArray.length - 1; i++) {
-        if(Number(numberArray[i + 1]) - Number(numberArray[i]) > 3 || Number(numberArray[i]) >= Number(numberArray[i + 1])) {
+        if(Number(numberArray[i]) - Number(numberArray[i + 1]) > 3 || Number(numberArray[i]) <= Number(numberArray[i + 1])) {
             state = "NotSafe"
             break
         }
@@ -24,80 +24,38 @@ const decreasingNumbers = (numberArray: string[]) => {
     return state
 }
 
-// Run through each line
 let safeCount: number = 0
+let newSafeCount: number = 0
 input.forEach((numbers) => {
-    const splitNumbers = numbers.split(" ")
+    let splitNumbers = numbers.split(" ")
     let state: string = ""
-    if(Number(splitNumbers[0]) > Number(splitNumbers[1]) && Number(splitNumbers[0]) - Number(splitNumbers[1]) <= 3) {
+    if(Number(splitNumbers[0]) < Number(splitNumbers[1]) && Number(splitNumbers[1]) - Number(splitNumbers[0]) <= 3) {
         state = increasingNumbers(splitNumbers)
     }
-    else if (Number(splitNumbers[0]) < Number(splitNumbers[1]) && Number(splitNumbers[1]) - Number(splitNumbers[0]) <= 3)  {
+    else if (Number(splitNumbers[0]) > Number(splitNumbers[1]) && Number(splitNumbers[0]) - Number(splitNumbers[1]) <= 3)  {
         state = decreasingNumbers(splitNumbers)
     }
 
-    
-    // Add to total
     if(state === "Safe") {
         safeCount++
     }
-})
 
-const newIncreasingNumbers = (numberArray: string[]) => {
-    let state: string = "Safe"
-    let removed: boolean = false
-    for(let i = 0; i < numberArray.length - 1; i++) {
-        if(Number(numberArray[i]) - Number(numberArray[i + 1]) > 3 || Number(numberArray[i]) <= Number(numberArray[i + 1])) {
-            if(!removed) {
-                removed = true
-                numberArray.splice(i,1)
-                i = i - 2
+    // Part 2 Check if can rerun with the subtracted parts or when original state returned null
+    if(state === "NotSafe" || state === "") {
+        for(let i = 0; i < numbers.split(" ").length; i++) {
+            splitNumbers = numbers.split(" ")
+            splitNumbers.splice(i,1)
+            if(Number(splitNumbers[0]) < Number(splitNumbers[1]) && Number(splitNumbers[1]) - Number(splitNumbers[0]) <= 3) {
+                state = increasingNumbers(splitNumbers)
             }
-            else {
-                state = "NotSafe"
+            else if (Number(splitNumbers[0]) > Number(splitNumbers[1]) && Number(splitNumbers[0]) - Number(splitNumbers[1]) <= 3)  {
+                state = decreasingNumbers(splitNumbers)
+            }
+            if(state === "Safe") {
                 break
             }
         }
     }
-    console.log("final", numberArray)
-    return state
-}
-
-const newDecreasingNumbers = (numberArray: string[]) => {
-    let state: string = "Safe"
-    let removed: boolean = false
-    for(let i = 0; i < numberArray.length - 1; i++) {
-        if(Number(numberArray[i + 1]) - Number(numberArray[i]) > 3 || Number(numberArray[i]) >= Number(numberArray[i + 1])) {
-            if(!removed) {
-                removed = true
-                numberArray.splice(i,1)
-                i = i -2
-            }
-            else {
-                state = "NotSafe"
-                break
-            }
-        }
-    }
-    console.log("final", numberArray)
-
-    return state
-}
-
-let newSafeCount: number = 0
-input.forEach((numbers) => {
-    const splitNumbers = numbers.split(" ")
-    console.log(splitNumbers)
-    let state: string = ""
-    if(Number(splitNumbers[0]) > Number(splitNumbers[1]) && Number(splitNumbers[0]) - Number(splitNumbers[1]) <= 3) {
-        state = newIncreasingNumbers(splitNumbers)
-    }
-    else if (Number(splitNumbers[0]) < Number(splitNumbers[1]) && Number(splitNumbers[1]) - Number(splitNumbers[0]) <= 3)  {
-        state = newDecreasingNumbers(splitNumbers)
-    }
-
-    console.log("state", state)
-
 
     if(state === "Safe") {
         newSafeCount++
